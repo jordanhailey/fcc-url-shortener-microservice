698f510306e4444a62177aebc062c99f6784d30a
Jordan Hailey <jordan.m.hailey@gmail.com>
2021-08-11 02:11:45 +0000

    creating api endpoint (no logic yet)

--- server.js
+++ server.js
@@ -19,6 +19,19 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// POST api/shorturl/:short_url endpoint
+app.post('/api/shorturl', function(req, res) {
+  console.log("write req body to memory")
+});
+
+// GET api/shorturl/:short_url endpoint
+app.get('/api/shorturl/:short_url', function(req, res) {
+  res.json({ redirect: {
+    from: req.params.short_url,
+    to: "lookup req and find endpoint to redirect to"
+  } });
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
